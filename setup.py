a5a36a36a495c8fffedc7da04b902b2ba3f4af4d
David Hotham <david.hotham@blueyonder.co.uk>
2022-06-11 14:45:05 +0100

    Fix requirements
    Allow python 3.6, require click 8

--- setup.py
+++ setup.py
@@ -10,19 +10,26 @@ setup(
     author = 'Egor Abramov',
     author_email = 'coreegor@gmail.com',
     version=version,
-    description="Bash 4.2 completion support for Click 8.0.x",
+    description="Bash 4.2 completion support for Click 8",
     py_modules=['click_bash42_completion'],
     include_package_data=True,
     long_description=long_description,
     long_description_content_type='text/markdown',
     download_url=f'https://github.com/coreegor/click-bash4.2-completion/archive/refs/tags/{version}.tar.gz',
-    python_requires='>=3.7',
+    install_requires=[
+        'click>=8.0.0,<9'
+    ],
+    python_requires='>=3.6',
     project_urls={
         'Source': 'https://github.com/coreegor/click-bash4.2-completion',
     },
     classifiers=[
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ]
 
 )
